e7634cdafa6a61cec10e2334e72e42701aea8af2
Niqoo10 <nicopanella2009@hotmail.com>
2023-04-20 18:08:03 -0300

    Fibonacci

---
+++ fibonacci.py
@@ -0,0 +1,20 @@
+
+def fibonacci(n):
+
+    if n == 0:
+        return []
+    
+    elif n == 1:
+        return [0]
+    
+    elif n == 2:
+        return [0, 1]
+    
+    else:
+        seq = [0, 1]
+
+        for i in range(2, n):
+            next_num = seq[i-1] + seq[i-2]
+            seq.append(next_num)
+            
+        return seq
